4edef2ddbb57e431db277ef8b235bdc75721424f
Anthony R. Lupica <arl127@uakron.edu>
2022-04-07 17:53:51 -0400

    Diff check

--- robot.js
+++ robot.js
@@ -12,10 +12,15 @@ function initCanvas()
 
     if (!gl) 
     { 
-        alert( "WebGL isn't available on your browser");
+        alert("WebGL isn't available on your browser");
     }
+    
+    // set viewing window
+    gl.viewport(0, 0, canvas.width, canvas.height);
     
     // set color of canvas (R, G, B, alpha "for opacity")
     gl.clearColor(0.50, 0.75, 0.75, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 }
+
+
