e85f191ae5cf13b2724d72299ae21818f520e4ff
Anthony R. Lupica <arl127@uakron.edu>
2022-04-07 16:37:17 -0400

    Initialize canvas and set canvas color

--- robot.js
+++ robot.js
@@ -1,4 +1,21 @@
+/*
+    include file for robot.html
+*/ 
+
 function initCanvas()
 {
-    console.log('initCanvas was called');
+    // console log for succesful call
+    console.log('initCanvas() was called');
+
+    var canvas = document.getElementById('robot-home');
+    var gl = WebGLUtils.setupWebGL(canvas);
+
+    if (!gl) 
+    { 
+        alert( "WebGL isn't available on your browser");
+    }
+    
+    // set color of canvas (R, G, B, alpha "for opacity")
+    gl.clearColor(0.50, 0.75, 0.75, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
 }
